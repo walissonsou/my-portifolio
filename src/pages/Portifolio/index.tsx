995580da3ba6a3148b4f74db821d1e0087114c5e
walissonsou <walissonsouza96@hotmail.com>
2022-11-01 22:45:53 -0300

    Iniciando com as funcionalidades da minha page, consumindo api do github

--- src/pages/Portifolio/index.tsx
+++ src/pages/Portifolio/index.tsx
@@ -1,9 +1,25 @@
+import { useCallback, useState } from "react";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/Profile/SearchInput";
 import { Post } from "./Post";
 import { PortifolioListContainer } from "./styles";
 
+interface IPost{
+  title: string;
+}
+
 export function Portifolio() {
+  const [ post, setPosts ] = useState<IPost[]>([]);
+
+  const getPosts = useCallback( async() => {
+    try{
+
+    } finally {
+
+    }
+
+  }, [posts])
+
   return (
     <>
       <Profile />
