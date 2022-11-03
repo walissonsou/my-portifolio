5d27562fc0de6c1cd0a068a7879a56a6f736ed26
walissonsou <walissonsouza96@hotmail.com>
2022-11-02 21:17:59 -0300

    deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,8 +5,8 @@ import { Portifolio } from './pages/Portifolio';
 export function Router() {
   return (
     <Routes>
-      <Route path="" element={<DefaultLayout />}>
-        <Route path="/" element={<Portifolio />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route element={<Portifolio />}/>        
       </Route>
     </Routes>
   );
