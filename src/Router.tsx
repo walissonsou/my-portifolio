61864d6884cb47ce44f366260d61bf7143398529
walissonsou <walissonsouza96@hotmail.com>
2022-11-02 22:00:19 -0300

    deploy2

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import { DefaultLayout } from './layouts/DefaultLayout';
 import { Portifolio } from './pages/Portifolio';
 
 export function Router() {
   return (
-    <Routes>      
-        
-        <Route path="/" element={<Portifolio />} />      
+    <Routes>
+      <Route path="" element={<Portifolio />} />
     </Routes>
   );
 }
